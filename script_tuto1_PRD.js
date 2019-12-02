31aaa15d697a78dc739c9b5869237bf9cc740933
Dune0lyn <43311890+Dune0lyn@users.noreply.github.com>
2019-12-02 16:53:53 +0100

    Create script_tuto1_PRD.js

---
+++ script_tuto1_PRD.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('#aquoicasert').click(function() {
+	  if ($('#aquoicasert').hasClass('d_clicked')) {
+            $('#aquoicasert').animate({backgroundColor:'#ff0000'} , 1000, function() { $('#aquoicasert').removeClass('d_clicked'); });
+	  } else {
+            $('#panneau_lat').animate({backgroundColor:'#0000FF'} , 1000, function() { $('#aquoicasert').addClass('d_clicked'); });
+	  }  
+	});
+});
