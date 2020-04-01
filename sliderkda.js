5095a6b2f538f5cf1abfdfe2f49ea439152552f9
Dune0lyn <43311890+Dune0lyn@users.noreply.github.com>
2020-04-01 14:13:19 +0200

    Update sliderkda.js

--- sliderkda.js
+++ sliderkda.js
@@ -1,4 +1,13 @@
 $(function(){
-	console.log('testestest');
-	
+	$(".checkboxkda").each(function(elem){
+		$(this).click(function(){
+			var contain=$(this).parent().parent().parent();
+			if($(this).is(':checked')){
+				console.log();
+				$(contain).removeClass('pan_main').addClass('pan_main_kda');
+			}else{
+				$(contain).removeClass('pan_main_kda').addClass('pan_main');
+			}
+		}); 
+	});	
 });
